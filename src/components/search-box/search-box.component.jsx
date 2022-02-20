8fdbab076ed8d373810ea6e654dfbb715516b637
Andres Viquez Chavarria <andresvvcc@gmail.com>
2022-02-20 13:48:30 -0600

    q

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./search-box.styles.css";
 
 export const SearchBox = ({ placeholder, handleChange }) => {
-    return(
-        <input
-            className="search"
-            type="search"
-            placeholder={placeholder}
-            onChange={handleChange}
-        />
-    );
-}
+  return (
+    <input
+      className="search"
+      type="search"
+      placeholder={placeholder}
+      onChange={handleChange}
+    />
+  );
+};
